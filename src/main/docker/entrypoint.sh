d3045c084dcb57fe63221e19a54456acc98ea33d
Filip Łazarski <filip.lazarski@hltech.com>
2019-02-28 18:17:32 +0100

    test

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -2,4 +2,4 @@
 java \
 -XX:+UnlockExperimentalVMOptions \
 -XX:+UseCGroupMemoryLimitForHeap \
-$* -jar ./judge-d-contract-publisher-0.1-SNAPSHOT.jar
+$* -jar ./judge-d-contract-publisher-0.1-SNAPSHOT.jar $0 $1 $2 $3 $4
