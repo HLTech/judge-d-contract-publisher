a5ef9996ba5b894fea8ee8175a04da57a77d39ec
Mateusz Urbański <mateusz.urbanski@hltech.com>
2019-05-08 10:36:12 +0200

    Change vaunt capabilities serialization format.

--- src/test/groovy/com/hltech/contracts/judged/publisher/capabilities/VauntCapabilitiesReaderSpec.groovy
+++ src/test/groovy/com/hltech/contracts/judged/publisher/capabilities/VauntCapabilitiesReaderSpec.groovy
@@ -24,16 +24,16 @@ class VauntCapabilitiesReaderSpec extends Specification {
             def capabilities = reader.read(properties)
         then:
             expect capabilities, isJson()
-            expect capabilities, hasJsonPath('$.contracts', hasSize(2))
+            expect capabilities, hasJsonPath('$', hasSize(2))
         and:
-            expect capabilities, hasJsonPath('$.contracts[0].destinationType', equalTo('QUEUE'))
-            expect capabilities, hasJsonPath('$.contracts[0].destinationName', equalTo('request_for_information_queue'))
-            expect capabilities, hasJsonPath('$.contracts[0].body.type', equalTo('object'))
-            expect capabilities, hasJsonPath('$.contracts[0].body.properties.name.type', equalTo('string'))
+            expect capabilities, hasJsonPath('$[0].destinationType', equalTo('QUEUE'))
+            expect capabilities, hasJsonPath('$[0].destinationName', equalTo('request_for_information_queue'))
+            expect capabilities, hasJsonPath('$[0].body.type', equalTo('object'))
+            expect capabilities, hasJsonPath('$[0].body.properties.name.type', equalTo('string'))
         and:
-            expect capabilities, hasJsonPath('$.contracts[1].destinationType', equalTo('TOPIC'))
-            expect capabilities, hasJsonPath('$.contracts[1].destinationName', equalTo('something_changed_topic'))
-            expect capabilities, hasJsonPath('$.contracts[1].body.type', equalTo('object'))
-            expect capabilities, hasJsonPath('$.contracts[1].body.properties.timestamp.type', equalTo('integer'))
+            expect capabilities, hasJsonPath('$[1].destinationType', equalTo('TOPIC'))
+            expect capabilities, hasJsonPath('$[1].destinationName', equalTo('something_changed_topic'))
+            expect capabilities, hasJsonPath('$[1].body.type', equalTo('object'))
+            expect capabilities, hasJsonPath('$[1].body.properties.timestamp.type', equalTo('integer'))
     }
 }
