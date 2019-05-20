6440c2575f852b97b2731791c0697fbfd1eb6818
Filip Łazarski <filip.lazarski@hltech.com>
2019-05-20 10:58:26 +0200

    Added vaunt model

--- src/main/java/com/hltech/contracts/judged/publisher/vaunt/Expectations.java
+++
@@ -1,9 +0,0 @@
-package com.hltech.contracts.judged.publisher.vaunt;
-
-import com.google.common.collect.Multimap;
-import lombok.Data;
-
-@Data
-public class Expectations {
-    private final Multimap<String, Contract> providerNameToContracts;
-}
